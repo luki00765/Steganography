7cd178abf45732ac6a08a35c630d24156c790195
Lukasz Paczynski <luki00765@gmail.com>
2015-03-07 14:50:12 +0100

    Steganography project

---
+++ Steganography/BoolVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Steganography
+{
+	class BoolVisibilityConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			bool isVisible = (bool)value;
+
+			if (isVisible)
+			{
+				return "Visible";
+			}
+			else
+			{
+				return "Collapsed";
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
